74f70a5ea394c64fb8cf81440eb72e147c142da3
shamsmirzaa <124146264+shamsmirzaa@users.noreply.github.com>
2025-07-24 22:44:57 +0530

    practice.dart

---
+++ daily_dart/dart_basic_day3/practice.dart
@@ -0,0 +1,27 @@
+void main() {
+  String firstName ='Shams';
+  String secondname='Mirza';
+  int age=22;
+  double height=5.9;
+  print("my name is "+firstName+" "+secondname);
+  print("my name is $firstName $secondname");
+
+}
+
+void main() {
+  String firstName ='Shams';
+  String secondname='Mirza';
+  int age=22;
+  double height=5.9;
+  print("my name is $firstName $secondname. I am $age years old and next year i will be ${age+1} years old. my height is $height");
+
+}
+
+void main(){
+  double height=5.9;
+  int age=22;
+  String friend1='max';
+  String friend2='jim';
+
+  print("There are 2 friends $friend1 and $friend2.\nBoth are $age years old but $friend1 is of the height $height and $friend2 is of the height ${height+2}");
+}
