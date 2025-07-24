6a2b37795e3cce5677ced21bde2397faa260c844
shamsmirzaa <124146264+shamsmirzaa@users.noreply.github.com>
2025-07-24 22:50:43 +0530

    Create q2_string_interpolation.dart

---
+++ daily_dart/dart_basic_day3/q2_string_interpolation.dart
@@ -0,0 +1,14 @@
+void main() {
+  String firstName ='Shams';
+  String secondname='Mirza';
+  print("my name is $firstName $secondname");
+}
+
+void main() {
+  String firstName ='Shams';
+  String secondname='Mirza';
+  int age=12;
+  double height=5.9;
+  print("my name is $firstName $secondname. I am $age years old and next year i will be ${age+1} years old. my height is $height");
+
+}
