a0e14fa959a6a44f4e29008928ff7abce1360063
shamsmirzaa <124146264+shamsmirzaa@users.noreply.github.com>
2025-07-26 23:11:05 +0530

    practice.dart

---
+++ daily_dart/dart_basic_day5/practice.dart
@@ -0,0 +1,14 @@
+## 🧪 Practice Time!
+
+```dart
+void main() {
+  String input = "100";
+  int number = int.parse(input);
+  double d = number.toDouble();
+  String againString = d.toString();
+
+  print("Int: $number");
+  print("Double: $d");
+  print("Back to string: $againString");
+}
+```
